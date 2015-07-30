c9851de0f594d31d7dcf8d1b091955e507f5dfea
James Traver <jtraverjr@gmail.com>
2015-07-29 21:51:34 -0500

    dat alter table tho

--- controllers/UsersController.rb
+++ controllers/UsersController.rb
@@ -1,3 +1,38 @@
 class UsersController < ApplicationController
 
+  post "/register" do
+
+    # generate a salt and hash to use
+    password_salt = BCrypt::Engine.generate_salt
+    password_hash = BCrypt::Engine.hash_secret(params[:password], password_salt)
+
+    new_user = UsersModel.new
+    new_user.user_name = params[:user_name]
+    new_user.user_email = params[:user_email]
+    new_user.password_salt = password_salt
+    new_user.password_hash = password_hash
+    new_user.is_admin = false
+
+    session[:user_name] = new_user.user_name
+
+    redirect "/"
+
+  end
+
+  post "/login" do
+    if userTable.has_key?(params[:username])
+      user = userTable[params[:username]]
+      if user[:passwordhash] == BCrypt::Engine.hash_secret(params[:password], user[:salt])
+        session[:username] = params[:username]
+        redirect "/"
+      end
+    end
+    erb :users_login_error
+  end
+
+  get "/logout" do
+    session[:username] = nil
+    redirect "/"
+  end
+
 end
--- migrations.sql
+++ migrations.sql
@@ -4,4 +4,4 @@ psql
 
 CREATE TABLE students (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255));
 
-CREATE TABLE users (id SERIAL PRIMARY KEY, user_name VARCHAR(255), user_email VARCHAR(255), password_hash VARCHAR(255), is_admin BOOLEAN);
+CREATE TABLE users (id SERIAL PRIMARY KEY, user_name VARCHAR(255), user_email VARCHAR(255), password_salt VARCHAR(255), password_hash VARCHAR(255), is_admin BOOLEAN);
