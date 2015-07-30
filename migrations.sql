5b4107bdb2ebe5bce69c2d58e5dadc653425586f
James Traver <jtraverjr@gmail.com>
2015-07-29 21:25:16 -0500

    migrations

---
+++ migrations.sql
@@ -0,0 +1,7 @@
+psql
+
+\c your_db_name_here
+
+CREATE TABLE students (id SERIAL PRIMARY KEY, name VARCHAR(255), email VARCHAR(255));
+
+CREATE TABLE users (id SERIAL PRIMARY KEY, user_name VARCHAR(255), user_email VARCHAR(255), password_hash VARCHAR(255), is_admin BOOLEAN);
